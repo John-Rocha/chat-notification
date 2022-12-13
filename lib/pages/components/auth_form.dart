4fcf031d31bf3f3743da121ada33fd9dc31b6188
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-13 13:42:06 -0300

    chore: form data with initial value

--- lib/pages/components/auth_form.dart
+++ lib/pages/components/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:chat_notification/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -8,6 +9,13 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _authFormData = AuthFormData();
+  final _formKey = GlobalKey<FormState>();
+
+  void _submit() {
+    final formValidate = _formKey.currentState?.validate() ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,26 +23,45 @@ class _AuthFormState extends State<AuthForm> {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Nome'),
-              ),
+              _authFormData.isSignup
+                  ? TextFormField(
+                      decoration: const InputDecoration(labelText: 'Nome'),
+                      initialValue: _authFormData.name,
+                      onChanged: (name) => _authFormData.name = name,
+                    )
+                  : const SizedBox.shrink(),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'E-mail'),
+                initialValue: _authFormData.email,
+                onChanged: (email) => _authFormData.email = email,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Senha'),
+                initialValue: _authFormData.password,
+                onChanged: (password) => _authFormData.password = password,
                 obscureText: true,
               ),
               const SizedBox(height: 12),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text('Entrar'),
+                onPressed: _submit,
+                child: Text(
+                  _authFormData.isLogin ? 'Entrar' : 'Cadastrar',
+                ),
               ),
               TextButton(
-                onPressed: () {},
-                child: const Text('Criar uma nova conta?'),
+                onPressed: () {
+                  setState(() {
+                    _authFormData.toggleAuthMode();
+                  });
+                },
+                child: Text(
+                  _authFormData.isLogin
+                      ? 'Criar uma nova conta?'
+                      : 'Já possui conta?',
+                ),
               ),
             ],
           ),
