750d903a42f399f92f420f5c20a32cf93824659c
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-15 20:24:40 -0300

    feat: implements PopupMenuButton in app bar

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_notification/components/messages.dart';
+import 'package:chat_notification/components/new_message.dart';
 import 'package:chat_notification/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,18 +9,41 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Chat Page'),
-            TextButton(
-              onPressed: () => AuthService().logout(),
-              child: const Text('Logout'),
-            )
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('App Chat'),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.black87,
+                    ),
+                    SizedBox(width: 10),
+                    Text('Sair')
+                  ],
+                ),
+                onTap: () {
+                  AuthService().logout();
+                },
+              ),
+            ],
+          )
+        ],
       ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Expanded(
+            child: Messages(),
+          ),
+          NewMessage(),
+        ],
+      )),
     );
   }
 }
