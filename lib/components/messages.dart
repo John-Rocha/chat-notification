bb1086011aaf9d45af21b7a19ed55bf0f4c1b658
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-15 21:15:16 -0300

    feat: treat data in the UI

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,4 @@
+import 'package:chat_notification/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +6,28 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mensagens'),
+    return StreamBuilder(
+      stream: ChatService().messagesStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text('Sem dados. Vamos conversar?'),
+          );
+        } else {
+          final messages = snapshot.data!;
+          return ListView.builder(
+            reverse: true,
+            itemCount: messages.length,
+            itemBuilder: (_, index) {
+              return Text(messages[index].text);
+            },
+          );
+        }
+      },
     );
   }
 }
