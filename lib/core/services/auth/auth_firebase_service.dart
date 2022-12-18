7efb7e0ff581b7eef9aab61b3ec2a730331ab382
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-18 20:21:48 -0300

    feat: configure data persistence in firestore

--- lib/core/services/auth/auth_firebase_service.dart
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:chat_notification/core/models/chat_user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -45,10 +46,14 @@ class AuthFirebaseService implements AuthService {
     final photoURL = await _uploadUserImage(image, imageName);
 
     // 2. Atualizar os atributos do usuário
-    credential.user?.updateDisplayName(name);
-    credential.user?.updatePhotoURL(photoURL);
+    await credential.user?.updateDisplayName(name);
+    await credential.user?.updatePhotoURL(photoURL);
 
-    // 3. Salvar dados no banco de dados
+    // 3. Salvar usuário no banco de dados (NoSQL)
+    await _saveChatUser(_toChatUser(
+      credential.user!,
+      photoURL,
+    ));
   }
 
   @override
@@ -67,12 +72,12 @@ class AuthFirebaseService implements AuthService {
     FirebaseAuth.instance.signOut();
   }
 
-  static ChatUser _toChatUser(User user) {
+  static ChatUser _toChatUser(User user, [String? imageURL]) {
     return ChatUser(
       id: user.uid,
       name: user.displayName ?? user.email!.split('@')[0],
       email: user.email!,
-      imageURL: user.photoURL ?? 'assets/images/avatar.png',
+      imageURL: imageURL ?? user.photoURL ?? 'assets/images/avatar.png',
     );
   }
 
@@ -85,5 +90,16 @@ class AuthFirebaseService implements AuthService {
     final imageRef = storage.ref().child('user_images').child(imageName);
     await imageRef.putFile(image).whenComplete(() {});
     return await imageRef.getDownloadURL();
+  }
+
+  Future<void> _saveChatUser(ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+    final docRef = store.collection('users').doc(user.id);
+
+    return docRef.set({
+      'name': user.name,
+      'email': user.email,
+      'imageURL': user.imageURL,
+    });
   }
 }
