209aaa60da48bf5076518bdfb815e68f188e6c55
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-18 17:20:18 -0300

    feat: changed firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:chat_notification/core/services/notification/chat_notification_service.dart';
-import 'package:chat_notification/firebase_options.dart';
 import 'package:chat_notification/pages/auth_or_app_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
